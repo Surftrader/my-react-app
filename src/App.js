5dec9716c51e664298dca14c2727c3c0416e64f0
Sergiy Poddubka <poseal@gmail.com>
2022-07-27 14:55:22 +0300

    add greeting by input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name, setName] = useState('')
+  return <div className="App">
+    <label className="label">Name</label>
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <p>Hi {name}</p>
+  </div>;
 }
 
 export default App;
