aecb2db9036bb44e108f219a836fd35b5655fc93
Sergiy Poddubka <poseal@gmail.com>
2022-08-02 16:13:47 +0300

    render page using useState hook

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [myDate, setMyDate] = useState(new Date())
 
   const increaseCounter = () => {
     setCounter((prev) => prev + 1)
     setCounter((prev) => prev + 1)
+    setMyDate(new Date())
   }
+
+  console.log('render')
 
   return (
     <div className="App" >
+      <div>{myDate.toISOString()}</div>
       {counter}
       <button type="button" onClick={increaseCounter}>Increase</button>
     </div>
