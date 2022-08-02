4faaaa22c63cba855b5119f9144f733e31c589be
Sergiy Poddubka <poseal@gmail.com>
2022-08-02 15:37:24 +0300

    onMouseEnter, onMouseLeave - example

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import './App.css'
 
 function App() {
-  const posts = [
-    {
-      id: 1,
-      title: 'News #1',
-      shortDescription: 'Short description of news #1'
-    },
-    {
-      id: 2,
-      title: 'News #2',
-      shortDescription: 'Short description of news #2'
-    },
-    {
-      id: 3,
-      title: 'News #3',
-      shortDescription: 'Short description of news #3'
-    }
-  ]
+  const sendForm = () => {
+    alert('Form was sent');
+  }
+  const mouseEnter = () => {
+    console.log('Mouse enter');
+  }
+  const mouseLeave = () => {
+    console.log('Mouse leave');
+  }
   return (
-    <div className="App">
-      {posts.map((post) => (
-        <div key={post.id.toString()}>
-          <h2>{post.title}</h2>
-          <p>{post.shortDescription}</p>
-        </div>
-      ))}
+    <div className="App" >
+      <button
+        type="button"
+        onClick={sendForm}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+      >
+        Send
+      </button>
     </div>
   );
 }
