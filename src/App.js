2ba27480d734b60a2f9ab51ede7560ba10fb40fc
Sergiy Poddubka <poseal@gmail.com>
2022-08-02 15:57:45 +0300

    preventDefault example

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      item: 'Go to the mall'
-    },
-    {
-      id: 2,
-      item: 'Pick up clothes from the dry cleaners'
-    },
-    {
-      id: 3,
-      item: 'Write todos'
-    },
-  ])
-
-  const deleteTodo = (id) => {
-    const newTodos = todos.filter((todo) => todo.id !== id)
-    setTodos(newTodos)
+  const sendForm = (e) => {
+    e.preventDefault();
+    console.log('Form was sent')
   }
-
   return (
     <div className="App" >
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id.toString()}>
-            {todo.item}
-            <button type="button" onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={sendForm}>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
