c8675793f6ffc5c3e3bec3e2b1a79122cfb8438e
Sergiy Poddubka <poseal@gmail.com>
2022-08-02 15:26:23 +0300

    loop with key - example 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css'
 
 function App() {
-  const num = [1, 2, 3, 4, 5]
-  return <div className="App">
-    <ul>
-      {num.map((value) => <li key={value.toString()}>{value}</li>)}
-    </ul>
-  </div>;
+  const posts = [
+    {
+      id: 1,
+      title: 'News #1',
+      shortDescription: 'Short description of news #1'
+    },
+    {
+      id: 2,
+      title: 'News #2',
+      shortDescription: 'Short description of news #2'
+    },
+    {
+      id: 3,
+      title: 'News #3',
+      shortDescription: 'Short description of news #3'
+    }
+  ]
+  return (
+    <div className="App">
+      {posts.map((post) => (
+        <div key={post.id.toString()}>
+          <h2>{post.title}</h2>
+          <p>{post.shortDescription}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
